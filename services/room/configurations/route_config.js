12c864195911c8391c4200f0183d9c6ff9d5cf1b
Carter Cobb <carter.cobb72@gmail.com>
2021-08-03 13:08:47 -0600

    added route configuration class

---
+++ services/room/configurations/route_config.js
@@ -0,0 +1,30 @@
+import Express from "express";
+import room from "../routes/room.js";
+
+/**
+ * Constructs routes from the imported object lists.
+ * Then assigns them to the instance of the Express app.
+ */
+export default class RouteConf {
+  /**
+   * Constructs and assigns the routes to the app instance
+   * @param {Express.Application} instance
+   */
+  constructor(instance) {
+    this.app = instance;
+    this.app.get("/", (_req, res) =>
+      res.status(200).json({ message: "Server Opperational" })
+    );
+    this.configureRoutes(room);
+  }
+
+  /**
+   * Constructs routes and assigns them to the app.
+   * @param {Array} routes
+   */
+  configureRoutes(routes) {
+    routes.forEach((route) =>
+      this.app[route.type].bind(this.app)(route.url, ...route.handlers)
+    );
+  }
+}
