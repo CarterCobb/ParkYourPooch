c6996b1a9cfd5ec6772c04c11f545cbc7063b7ae
Carter Cobb <carter.cobb72@gmail.com>
2021-08-03 13:04:15 -0600

    linked order routes

--- services/order/configurations/route_config.js
+++ services/order/configurations/route_config.js
@@ -15,7 +15,7 @@ export default class RouteConf {
     this.app.get("/", (_req, res) =>
       res.status(200).json({ message: "Server Opperational" })
     );
-    this.configureRoutes(employee);
+    this.configureRoutes(order);
   }
 
   /**
