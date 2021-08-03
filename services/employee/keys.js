11df05aaf772f794ed03126e51313ffcc62cecf0
Carter Cobb <carter.cobb72@gmail.com>
2021-08-03 16:52:35 -0600

    added JWT secrets

--- services/employee/keys.js
+++ services/employee/keys.js
@@ -5,3 +5,6 @@ config();
 export const NODE_ENV = process.env.NODE_ENV;
 // MongoDB
 export const MONGO_URL = process.env.MONGO_URL;
+// JWT
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
