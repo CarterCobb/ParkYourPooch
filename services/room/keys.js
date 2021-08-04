15133364d9311edcaa7d7a526f067fb768e83c59
Carter Cobb <carter.cobb72@gmail.com>
2021-08-04 13:09:45 -0600

    added test users

--- services/room/keys.js
+++ services/room/keys.js
@@ -5,3 +5,20 @@ config();
 export const NODE_ENV = process.env.NODE_ENV;
 // MongoDB
 export const MONGO_URL = process.env.MONGO_URL;
+// JWT
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+// Test Data
+export const TEST_CUSTOMER = {
+  _id: 12345,
+  name: "test",
+  email: "test@test.com",
+  password: "12345",
+  role: "CUSTOMER",
+  pooches: [],
+};
+export const TEST_EMPLOYEE = {
+  _id: 12345,
+  name: "Billy",
+  password: "12345",
+  role: "EMPLOYEE",
+};
