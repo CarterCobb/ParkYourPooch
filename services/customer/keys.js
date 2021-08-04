3461122b57f32303dbedf15288d870c6d3f430e8
Carter Cobb <carter.cobb72@gmail.com>
2021-08-03 23:40:40 -0600

    added test customer

--- services/customer/keys.js
+++ services/customer/keys.js
@@ -8,3 +8,10 @@ export const MONGO_URL = process.env.MONGO_URL;
 // JWT
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+// Test Data
+export const TEST_CUSTOMER = {
+  _id: 12345,
+  name: "Billy",
+  password: "12345",
+  role: "EMPLOYEE",
+};
