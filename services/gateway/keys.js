c5cc29fd509936d9ae07cbf95c6d13b21e049e4e
Carter Cobb <carter.cobb72@gmail.com>
2021-08-05 12:33:45 -0600

    added JWT token secrets to keys

--- services/gateway/keys.js
+++ services/gateway/keys.js
@@ -3,3 +3,6 @@ const { config } = dotenv;
 config();
 // Node
 export const NODE_ENV = process.env.NODE_ENV;
+// JWT
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
