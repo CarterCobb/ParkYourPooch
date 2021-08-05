eb64ed43c07d1522e503987d4596d9ef699e1f24
Carter Cobb <carter.cobb72@gmail.com>
2021-08-05 13:14:18 -0600

    added pages & page styles

---
+++ services/client/src/pages/employee-dash.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class EmployeeDash extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Employee Dashboard</div>;
+  }
+}
