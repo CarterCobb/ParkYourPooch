3af767c13172bbb281be77f1f51fac138c422bd4
Carter Cobb <carter.cobb72@gmail.com>
2021-08-04 19:14:09 -0600

    configures routes

--- services/gateway/app.mjs
+++ services/gateway/app.mjs
@@ -3,6 +3,7 @@ import cors from "cors";
 import cluster from "cluster";
 import os from "os";
 import { NODE_ENV } from "./keys.js";
+import Routes from "./configurations/route_config.js";
 const cpus = os.cpus().length;
 const app = express();
 
@@ -18,9 +19,7 @@ if (NODE_ENV !== "test" && cluster.isMaster && cpus > 1) {
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
   app.use(express.json({ limit: "50mb" }));
   app.use(cors());
-  app.get("/", (_req, res) =>
-    res.status(200).json({ message: "Server Opperational" })
-  );
+  Routes.registerTo(app);
   app.listen(1000, () =>
     console.log(`[${process.pid}] listening on port 1000`)
   );
