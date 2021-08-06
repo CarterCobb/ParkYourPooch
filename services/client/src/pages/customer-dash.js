cc8037b80f462b830e5066223b60483bd9e0f499
Carter Cobb <carter.cobb72@gmail.com>
2021-08-06 15:36:05 -0600

    added employee + customer portal *generic*

--- services/client/src/pages/customer-dash.js
+++ services/client/src/pages/customer-dash.js
@@ -1,12 +1,85 @@
 import React, { Component } from "react";
+import { Layout, Menu, Button } from "antd";
+import User from "../api/user";
+import { FaDog, FaBook, FaCog } from "react-icons/fa";
+import "../styles/dash.css";
+const { Header, Content, Sider } = Layout;
 
 export default class CustomerDash extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      content: null,
+    };
   }
 
   render() {
-    return <div>Customer Dashbaord</div>;
+    const logout = () => User.logout(() => (window.location.href = "/"));
+    const changeContent = ({ key }) => {
+      switch (key) {
+        case "1":
+          return this.setState({ content: <div>Bookings</div> });
+        case "2":
+          return this.setState({ content:  <div>Pooches</div> });
+        case "3":
+          return this.setState({ content:  <div>Settings</div> });
+        default:
+          return this.setState({ content:  <div>Bookings</div> });
+      }
+    };
+    return (
+      <Layout className="layout">
+        <Header className="header-bar">
+          <div className="logo" />
+          <h1>Park Your Pooch</h1>
+          <Button
+            shape="round"
+            size="large"
+            type="primary"
+            onClick={logout}
+            className="header-menu-login"
+          >
+            Logout
+          </Button>
+        </Header>
+        <Layout>
+          <Sider width={220} className="portal-sider">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              style={{ height: "100%", borderRight: 0 }}
+              onSelect={changeContent}
+            >
+              <Menu.Item
+                key="1"
+                icon={<FaBook size={25} />}
+                className="portal-menu-item"
+              >
+                Bookings
+              </Menu.Item>
+              <Menu.Item
+                key="2"
+                icon={<FaDog size={25} />}
+                className="portal-menu-item"
+              >
+                Pooches
+              </Menu.Item>
+              <Menu.Item
+                key="3"
+                icon={<FaCog size={25} />}
+                className="portal-menu-item"
+              >
+                Settings
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content className="portal-main-content">
+              {this.state.content}
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    );
   }
 }
--- services/client/src/pages/employee-dash.js
+++ services/client/src/pages/employee-dash.js
@@ -1,12 +1,85 @@
 import React, { Component } from "react";
+import { Layout, Menu, Button } from "antd";
+import User from "../api/user";
+import { FaHome, FaBox, FaCog } from "react-icons/fa";
+import "../styles/dash.css";
+const { Header, Content, Sider } = Layout;
 
 export default class EmployeeDash extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      content: null,
+    };
   }
 
   render() {
-    return <div>Employee Dashboard</div>;
+    const logout = () => User.logout(() => (window.location.href = "/"));
+    const changeContent = ({ key }) => {
+      switch (key) {
+        case "1":
+          return this.setState({ content: <div>Orders</div> });
+        case "2":
+          return this.setState({ content: <div>Rooms</div> });
+        case "3":
+          return this.setState({ content: <div>Settings</div> });
+        default:
+          return this.setState({ content: <div>Orders</div> });
+      }
+    };
+    return (
+      <Layout className="layout">
+        <Header className="header-bar">
+          <div className="logo" />
+          <h1>Park Your Pooch</h1>
+          <Button
+            shape="round"
+            size="large"
+            type="primary"
+            onClick={logout}
+            className="header-menu-login"
+          >
+            Logout
+          </Button>
+        </Header>
+        <Layout>
+          <Sider width={220} className="portal-sider">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              style={{ height: "100%", borderRight: 0 }}
+              onSelect={changeContent}
+            >
+              <Menu.Item
+                key="1"
+                icon={<FaBox size={25} />}
+                className="portal-menu-item"
+              >
+                Orders
+              </Menu.Item>
+              <Menu.Item
+                key="2"
+                icon={<FaHome size={25} />}
+                className="portal-menu-item"
+              >
+                Rooms
+              </Menu.Item>
+              <Menu.Item
+                key="3"
+                icon={<FaCog size={25} />}
+                className="portal-menu-item"
+              >
+                Settings
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Content className="portal-main-content">
+              {this.state.content}
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+    );
   }
 }
