63f7841514a18a56ac6b8214979f695e159e2530
Carter Cobb <carter.cobb72@gmail.com>
2021-08-06 18:55:24 -0600

    add pooch function

--- services/client/src/api/customer.js
+++ services/client/src/api/customer.js
@@ -1,6 +1,7 @@
 import { api } from "./helpers/network-prefix";
 import axios from "axios";
 import User from "./user";
+import { ls } from "./user";
 
 export default class Customer {
   /**
@@ -19,6 +20,43 @@ export default class Customer {
       cb(null);
     } catch (error) {
       cb({ error: error.message });
+    }
+  }
+
+  /**
+   * Adds a pooch to the customer object
+   * @param {String} pooch pooch id
+   * @param {Function} cb callback function (user, err)
+   * @returns
+   */
+  static async addPooch(pooch, cb) {
+    try {
+      const token = ls.get("token");
+      const put = await axios.put(
+        `${api}/customer/pooch/${pooch}`,
+        {},
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      if (!put.data._embedded.customer) {
+        try {
+          await User.getTokens((err) => {
+            if (err) return cb(null, err);
+          });
+          const token2 = ls.get("token");
+          const put2 = await axios.put(
+            `${api}/customer/pooch/${pooch}`,
+            {},
+            { headers: { Authorization: `Bearer ${token2}` } }
+          );
+          if (!put2.data._embedded.user) return cb(null, put2.data._embedded);
+          cb(put2.data._embedded.customer, null);
+        } catch (error) {
+          cb(null, { error: error.message });
+        }
+      }
+      cb(put.data._embedded.customer, null);
+    } catch (error) {
+      cb(null, { error: error.message });
     }
   }
 }
