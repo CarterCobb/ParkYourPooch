be00dfb865a4421493bbb5fda0d41c3e9df3c0b4
Carter Cobb <carter.cobb72@gmail.com>
2021-08-06 20:21:22 -0600

    remove pooch function

--- services/client/src/api/customer.js
+++ services/client/src/api/customer.js
@@ -59,4 +59,37 @@ export default class Customer {
       cb(null, { error: error.message });
     }
   }
+
+  /**
+   * Removes a pooch from the customer object
+   * @param {String} pooch pooch id
+   * @param {Function} cb callback function (user, err)
+   * @returns
+   */
+  static async removePooch(pooch, cb) {
+    try {
+      const token = ls.get("token");
+      const del = await axios.delete(`${api}/customer/pooch/${pooch}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (!del.data._embedded.customer) {
+        try {
+          await User.getTokens((err) => {
+            if (err) return cb(null, err);
+          });
+          const token2 = ls.get("token");
+          const del2 = await axios.delete(`${api}/customer/pooch/${pooch}`, {
+            headers: { Authorization: `Bearer ${token2}` },
+          });
+          if (!del2.data._embedded.user) return cb(null, del2.data._embedded);
+          cb(del2.data._embedded.customer, null);
+        } catch (error) {
+          cb(null, { error: error.message });
+        }
+      }
+      cb(del.data._embedded.customer, null);
+    } catch (error) {
+      cb(null, { error: error.message });
+    }
+  }
 }
--- services/client/src/api/pooch.js
+++ services/client/src/api/pooch.js
@@ -1,6 +1,8 @@
 import { api } from "./helpers/network-prefix";
 import axios from "axios";
 import Customer from "./customer";
+import User from "./user";
+import { ls } from "./user";
 
 export default class Pooch {
   /**
@@ -34,6 +36,42 @@ export default class Pooch {
       return get.data._embedded.pooch;
     } catch (_e) {
       return null;
+    }
+  }
+
+  /**
+   * Removes a pooch from the database and the customer object
+   * @param {String} id pooch id to remove
+   * @param {Function} cb callback function (user, err)
+   */
+  static async deletePoochById(id, cb) {
+    try {
+      const token = ls.get("token");
+      const del = await axios.delete(`${api}/pooch/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (del.status !== 204) {
+        try {
+          await User.getTokens((err) => {
+            if (err) return cb(null, err);
+          });
+          const token2 = ls.get("token");
+          const del2 = await axios.delete(`${api}/pooch/${id}`, {
+            headers: { Authorization: `Bearer ${token2}` },
+          });
+          if (del2.status !== 204) return cb(null, del2.data._embedded);
+          await Customer.removePooch(id, (user, err) => {
+            return cb(user, err);
+          });
+        } catch (error) {
+          cb(null, { error: error.message });
+        }
+      }
+      await Customer.removePooch(id, (user, err) => {
+        return cb(user, err);
+      });
+    } catch (error) {
+      cb(null, { error: error.message });
     }
   }
 }
