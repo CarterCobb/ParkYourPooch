0082d1c3e0485f71821b2adccde3a51c9deb27a5
Carter Cobb <carter.cobb72@gmail.com>
2021-08-06 18:57:08 -0600

    saves user to redux store

--- services/client/src/components/protected-route.js
+++ services/client/src/components/protected-route.js
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Spin } from "antd";
 import { Route, Redirect } from "react-router-dom";
 import User from "../api/user";
+import { connect } from "react-redux";
+import { setUser as setUserStore } from "../redux/userActions";
 import "../styles/protected-route.css";
 
 /**
  * Protects routes from global use.
  * @param {Object} `Object gets descructed~
  */
-const ProtectedRoute = ({ component: Component, ...rest }) => {
+const ProtectedRoute = ({ component: Component, dispatch, ...rest }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -16,13 +18,16 @@ const ProtectedRoute = ({ component: Component, ...rest }) => {
     if (mounted) {
       User.getUser((user, err) => {
         if (err) setUser("NO_AUTH");
-        else setUser(user);
+        else {
+          setUser(user);
+          dispatch(setUserStore(user));
+        }
       });
     }
     return () => {
       mounted = false;
     };
-  }, []);
+  }, [dispatch]);
 
   if (user === null)
     return (
@@ -50,4 +55,8 @@ const ProtectedRoute = ({ component: Component, ...rest }) => {
   );
 };
 
-export default ProtectedRoute;
+const mapStateToProps = (state) => {
+  const { user } = state;
+  return { user };
+};
+export default connect(mapStateToProps)(ProtectedRoute);
