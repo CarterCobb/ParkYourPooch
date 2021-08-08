59b8a322b084e49cbfbe653abb9983510bbce072
Carter Cobb <carter.cobb72@gmail.com>
2021-08-08 14:29:31 -0600

    create room function

--- services/client/src/api/rooms.js
+++ services/client/src/api/rooms.js
@@ -15,4 +15,21 @@ export default class Room {
       cb({ error: error.message });
     }
   }
+
+  /**
+   * Creates a room
+   * @param {Object} payload
+   * @param {Function} cb callback function (err)
+   */
+  static async createRoom(payload, cb) {
+    try {
+      const post = await axios.post(`${api}/room`, JSON.stringify(payload), {
+        headers: { "Content-Type": "application/json" },
+      });
+      if (post.status !== 200) cb(post.data._embedded);
+      cb(null);
+    } catch (error) {
+      cb({ error: error.message });
+    }
+  }
 }
