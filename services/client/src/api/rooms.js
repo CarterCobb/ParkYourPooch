5c284bfc467085182982d3efa2b2c464454168e5
Carter Cobb <carter.cobb72@gmail.com>
2021-08-08 16:20:49 -0600

    add a booking to a room function

--- services/client/src/api/rooms.js
+++ services/client/src/api/rooms.js
@@ -1,5 +1,6 @@
 import { api } from "./helpers/network-prefix";
 import axios from "axios";
+import User, { ls } from "./user";
 
 export default class Room {
   /**
@@ -27,6 +28,53 @@ export default class Room {
         headers: { "Content-Type": "application/json" },
       });
       if (post.status !== 200) cb(post.data._embedded);
+      cb(null);
+    } catch (error) {
+      cb({ error: error.message });
+    }
+  }
+
+  /**
+   * Adds a booking to a room *call after completing order*
+   * @param {String} room room id
+   * @param {Object} payload booking object
+   * @param {Function} cb callback function (err)
+   */
+  static async addBooking(room, payload, cb) {
+    try {
+      const token = ls.get("token");
+      const put = await axios.put(
+        `${api}/room/${room}/book`,
+        JSON.stringify(payload),
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (put.status !== 200) {
+        try {
+          await User.getTokens((err) => {
+            if (err) return cb(err);
+          });
+          const token2 = ls.get("token");
+          const put2 = await axios.put(
+            `${api}/room/${room}/book`,
+            JSON.stringify(payload),
+            {
+              headers: {
+                Authorization: `Bearer ${token2}`,
+                "Content-Type": "application/json",
+              },
+            }
+          );
+          if (put2.status !== 200) return cb(put2.data._embedded);
+          cb(null);
+        } catch (error) {
+          cb({ error: error.message });
+        }
+      }
       cb(null);
     } catch (error) {
       cb({ error: error.message });
