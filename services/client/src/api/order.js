f98ebac4f29d8ae6116c4ad66d220a2f3c03f54d
Carter Cobb <carter.cobb72@gmail.com>
2021-08-08 22:35:26 -0600

    get all orders function

--- services/client/src/api/order.js
+++ services/client/src/api/order.js
@@ -1,6 +1,5 @@
 import { api } from "./helpers/network-prefix";
 import axios from "axios";
-import Customer from "./customer";
 import User from "./user";
 import { ls } from "./user";
 
@@ -18,7 +17,41 @@ export default class Order {
       if (post.status !== 201) return cb(post.data._embedded);
       cb(null);
     } catch (error) {
-      cb({ error: error.response ? error.response.data._embedded.error : error.message });
+      cb({
+        error: error.response
+          ? error.response.data._embedded.error
+          : error.message,
+      });
+    }
+  }
+
+  /**
+   * Get all the orders
+   * @param {Function} cb callbacj function (orders, err)
+   */
+  static async getAllOrders(cb) {
+    try {
+      const token = ls.get("token");
+      const get = await axios.get(`${api}/orders`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      if (get.status !== 200) {
+        try {
+          await User.getTokens((err) => {
+            if (err) return cb(null, err);
+          });
+          const token2 = ls.get("token");
+          const get2 = await axios.get(`${api}/poochs`, {
+            headers: { Authorization: `Bearer ${token2}` },
+          });
+          if (get2.status !== 200) return cb(null, get2.data._embedded);
+          cb(get2.data._embedded.orders, null);
+        } catch (error) {
+          cb(null, { error: error.message });
+        }
+      } else cb(get.data._embedded.orders, null);
+    } catch (error) {
+      cb(null, { error: error.message });
     }
   }
 }
